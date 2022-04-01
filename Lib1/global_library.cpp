fda35c2503b22f8c5958fe5c88a0f3b3b5959f72
AlexanderRyzhakov <rizhakovalexandr@yandex.ru>
2022-04-01 19:06:12 +0300

    Delete Lib1 directory

--- Lib1/global_library.cpp
+++
@@ -1,8 +0,0 @@
-#include "global_library.h"
-#include "library_main_class.h"
-
-Library library("default");
-const std::vector<std::string> kVersion = {
-    "ALI_APP_v_0.2",
-    "ALI_APP_v_0.1"
-}; //actaul at begin(), older to end()
